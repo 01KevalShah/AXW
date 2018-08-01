b50c82b002c56973d555a154f7dff454b519a177
Keval Shah <kevalshah@Kevs-Mac.local>
2018-07-31 19:06:01 -0700

    first commit

---
+++ src/containers/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1></h1> // create title for the main page
+          <p></p> // create a subheading for the main page
+        </div>
+      </div>
+    );
+  }
+}
+
